a80073ac96c5d4accc4afe33ec7e92c1270380fc
emma-dougherty <doughertyemma@hotmail.com>
2022-04-07 13:24:24 +0100

    second commit

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,2 +1,14 @@
-def return_string(number):
-    return str(number)
+# def return_string(number):
+#     return str(number)
+
+def fizzbuzz(input_number):
+    # for number in range(0, 50):
+    if input_number % 3 == 0 and input_number % 5 == 0:
+        return "FizzBuzz"
+    
+    if input_number % 3 == 0:
+        return "Fizz"
+    elif input_number % 5 == 0:
+        return "Buzz"
+    else:
+        return str(input_number)
--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -5,32 +5,32 @@ import unittest
 class TestFizzbuzz(unittest.TestCase):
 
     def test_return_integer_1_as_string(self):
-        return_integer_as_string = return_string(1)
+        return_integer_as_string = fizzbuzz(1)
         self.assertEqual( "1", return_integer_as_string)
     
-    # def test_return_integer_2_as_string(self):
-    #     return_integer_as_string = return_string(2)
-    #     self.assertEqual( "2", return_integer_as_string)
+    def test_return_integer_2_as_string(self):
+        return_integer_as_string = fizzbuzz(2)
+        self.assertEqual( "2", return_integer_as_string)
 
-    # def test_return_fizz_if_divisible_by_3(self):
-    #     return_fizz_if_divisible_by_3 = return_fizz(3)
-    #     self.assertEqual("Fizz", return_fizz_if_divisible_by_3)    
+    def test_return_fizz_if_divisible_by_3(self):
+        return_fizz_if_divisible_by_3 = fizzbuzz(3)
+        self.assertEqual("Fizz", return_fizz_if_divisible_by_3)    
 
-    # def test_return_integer_4_as_string(self):
-    #     return_integer_as_string = return_string(4)
-    #     self.assertEqual("4", return_integer_as_string)
+    def test_return_integer_4_as_string(self):
+        return_integer_as_string = fizzbuzz(4)
+        self.assertEqual("4", return_integer_as_string)
     
-    # def test_return_buzz_if_divisible_by_5(self):
-    #     return_buzz_if_divisible_by_5 = return_buzz(5)
-    #     self.assertEqual("Buzz", return_buzz_if_divisible_by_5)
+    def test_return_buzz_if_divisible_by_5(self):
+        return_buzz_if_divisible_by_5 = fizzbuzz(5)
+        self.assertEqual("Buzz", return_buzz_if_divisible_by_5)
     
-    # def test_if_not_divisible_by_3_and_5_return_integer_as_string(self):
-    #     if_not_divisible_by_3_and_5_return_integer_as_string = return_string_if_divisible_by_3_and_5(7)
-    #     self.assertEqual("7", if_not_divisible_by_3_and_5_return_integer_as_string)
+    def test_if_not_divisible_by_3_and_5_return_integer_as_string(self):
+        if_not_divisible_by_3_and_5_return_integer_as_string = fizzbuzz(7)
+        self.assertEqual("7", if_not_divisible_by_3_and_5_return_integer_as_string)
 
-    # def test_return_fizzbuzzz_if_divisible_by_3_and_5(self):
-    #     return_fizzbuzzz_if_divisible_by_3_and_5 = return_fizzbuzz(15)
-    #     self.assertEqual("FizzBuzz", return_fizzbuzzz_if_divisible_by_3_and_5)
+    def test_return_fizzbuzzz_if_divisible_by_3_and_5(self):
+        return_fizzbuzzz_if_divisible_by_3_and_5 = fizzbuzz(15)
+        self.assertEqual("FizzBuzz", return_fizzbuzzz_if_divisible_by_3_and_5)
 
 
 # test 1 : to take in the integer 1 and to return the integer as a string - "1"
